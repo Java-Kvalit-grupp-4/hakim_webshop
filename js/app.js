acd39a8ffe9b237e7131f400d823d95dd698bb7d
Oscar Forss <oscar.forss.1@gmail.com>
2021-03-31 11:52:34 +0200

    fixed checkbox

--- js/app.js
+++ js/app.js
@@ -2,8 +2,9 @@ let productList = [];
 let customerList = [];
 
 // test data urls
-let productTestUrl = './TestData/test_data_v.1.0.JSON' 
-let customerTestUrl = './TestData/testdata_persons.json'
+//let productTestUrl = './TestData/test_data_v.1.0.JSON' 
+// let customerTestUrl = './TestData/testdata_persons.json'
+//let cartTestUrl = './TestData/test_data_cart.JSON' 
 
 /**
  * [fetches data from url]
@@ -16,14 +17,12 @@ function load(url){
     .then(response => response.json())
     .then(data => data.forEach(e => {
             list.push(e)
+            
     }))
+    console.log('in load');
+    console.log(list)
     return list;
 }
 
-// when page is loaded
-productList = load(productTestUrl)
-customerList = load(customerTestUrl)
-console.log(productList)
-console.log(customerList)
 
 
