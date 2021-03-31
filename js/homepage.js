59d4a0ba75ab981a0d0fe80cdb205dfa4f562ff9
Oscar Forss <oscar.forss.1@gmail.com>
2021-03-31 15:02:01 +0200

    added productNr ti test data

--- js/homepage.js
+++ js/homepage.js
@@ -1,8 +1,61 @@
+
+
+
+let products = [];
+    $(document).ready(function () {
+      load();
+      
+    });
+
+    function load() {
+      fetch("./TestData/test_data_v.1.0.JSON")
+        .then((response) => response.json())
+        .then((data) => render(data))
+        .catch((error) => console.error(error));
+    }
+
+    function render(data) {
+      products = data;
+
+      getProducts(products);
+     
+      let categories = [];
+      products.forEach(element => {
+        categories.push(element.category)
+      });
+
+      let uniqueCategories = [...new Set(categories)];
+
+      uniqueCategories.forEach(element => {
+      $("#sidomeny").append(`
+          <button id= "${element}" type="button" class="list-group-item list-group-item-action fs-2" style="background-color:wheat ;">${element}</button>`
+      );
+    });
+
+    $("#sidomeny button").on("click", function(){
+      let btnId = $(this).attr("id");
+      let list = [];
+     products.forEach(element => {
+       if (element.category == btnId){
+           list.push(element);
+          }
+        });
+         
+        $("#products").empty();
+        getProducts(list);
+
+        $("#all").click(function(){
+          getProducts(products);
+        });
+            });
+        }
+
 function getProducts(list) {
     list.forEach(element => {
+      console.log(element)
         $("#products").append(`
         <div class="col-sm-3 pb-5">
-          <div class="card text-center h-100" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2)">
+          <div id=""class="card text-center h-100" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2)">
             <img src="${element.image}" alt="img" class="card-img-top pt-5 ps-5 pe-5">
             <div class="card-body d-flex flex-column">
               <h3 class="card-title" style="font-weight: bold;">${element.title} 8</h3>
@@ -12,11 +65,18 @@ function getProducts(list) {
                 <button type="button" class="btn btn-outline-dark add1btn d-inline me-1" >-</button>
                   <div id="amount" class="d-inline">1</div>
                   <button type="button" class="btn btn-outline-dark add1btn d-inline ms-1">+</button>
-                  <button id="btn1" type="button" class="btn btn-lg btn-block btn-outline-dark align-self ms-5" style="margin-top: auto">Lägg till varukorg</button></p>
+                  <button id="btn1" type="button" class="btn btn-lg btn-block btn-outline-dark align-self ms-5 add-product-to-cart" style="margin-top: auto">Lägg till varukorg</button></p>
               </div>
             </div>
           </div>
         </div>`
         );
       });
+
+      $.each($('.add-product-to-cart'),function( index, value ) {
+        value.addEventListener('click',(e) => {
+          console.log(e.target)
+        })
+      })
 }
+
