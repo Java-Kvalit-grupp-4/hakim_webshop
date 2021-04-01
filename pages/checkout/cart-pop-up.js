a3ced5c666990d5dd08d139870e3a65c83a31e9a
Christoffer Grännby <cribbster1987@hotmail.com>
2021-04-01 13:32:44 +0200

    first commit

--- pages/checkout/cart-pop-up.js
+++ pages/checkout/cart-pop-up.js
@@ -9,10 +9,9 @@ function load() {
   }
 
 function renderCart(data){
-    const getProducts = JSON.parse(localStorage.getItem("cart"))
+    var getProducts = JSON.parse(localStorage.getItem("cart"))
 
     data.forEach(element => {
-        console.log("funkar?")
         document.getElementById("productsInCart").innerHTML +=
         `<tr class="table-light center-table">
                 <td><button type="button" class="btn btn-light"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
@@ -21,12 +20,11 @@ function renderCart(data){
                 <td scope="row">${element.title}</td>
                 <td scope="row"><img class="cart-thumbnail" src="${element.image} alt="Product picture"</td>
                 <td scope="row">${element.price}</td>
-                <td><button type="button" class="btn btn-dark" onclick="removeProduct(${element.id})">-
-                    <div id="quantityInCart">
-                        <p>0</p>
-                    </div>
-                    <button type="button" class="btn brn-dark" onclick=addProduct(${element.id})">+
-                </td>
+                <div>
+                <td><button type="button" class="shadow-button" onclick="removeProduct(${element.id})">-</td>    
+                <td><div id="quantityInCart"> 1 </div></td>
+                    <td><button type="button" class="shadow-button" onclick=addProduct(${element.id})">+</td>
+                </div>
                 <td id="${element.id}">${(element.price * element.quantity).toFixed(2)}</td>
             </tr>`
     });
@@ -51,3 +49,32 @@ function removeProduct(product) {
         localStorage.setItem("cart", JSON.stringify(removing));
       }
     }
+
+      // Get the modal
+  var modal = document.getElementById("myModal");
+  
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn");
+  
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+  
+  // When the user clicks the button, open the modal 
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+  
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+ 
+    
