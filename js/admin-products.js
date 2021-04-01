4052bb6936f33d746a2863c06dd79c48aec1e006
I-Zor <ivona.zoricic@gmail.com>
2021-04-01 13:34:17 +0200

    Admin produktkatalog + admin produktsida

--- js/admin-products.js
+++ js/admin-products.js
@@ -1,8 +1,12 @@
-function getProducts(list) {
+/**
+ * Generates a table with products
+ * @param {Array} list - Webshop products to be displayed on the page  
+ */
+function showProducts(list) {
     list.forEach(element => {
         let round = parseInt(element.price).toFixed(2);
         $("#products").append(
-            `<tr>
+            `<tr id="${element.id}">
               <td>${element.id}</td>
               <td ><h5 >${element.title}</h5></td>
               <td ><h5 >Hakim</h5></td>
