0673a8b2483cc70ad0fc84547ad258ff74bcac96
Oscar Forss <oscar.forss.1@gmail.com>
2021-04-06 11:12:16 +0200

    added function to trashcan button

--- pages/checkout/cart-pop-up.js
+++ pages/checkout/cart-pop-up.js
@@ -1,25 +1,35 @@
-let cart = JSON.parse(localStorage.getItem("cart"));
+/* let cart = JSON.parse(localStorage.getItem("cart"));
 if (cart == null) {
   cart = [];
-}
+} */
+
+let cart = [];
+
+$(document).ready(load)
 
 function load() {
     let cartTestUrl = '../../TestData/test_data_cart.JSON'
     fetch(cartTestUrl)
       .then((response) => response.json())
-      .then((data) => renderCart(data))
+      .then((data) => makeArrayFromData(data))
       .catch((error) => console.error(error));
-  }
 
-function renderCart(data){
-    var getProducts = JSON.parse(localStorage.getItem("cart"))
+      renderCart()
+}
 
-    // centrera papperskorgen och produktantal i cellerna
-    // ta bort borders på produktantal ??
-    data.forEach(element => {
+function makeArrayFromData(data) {
+  cart = data
+  localStorage.setItem("cart", JSON.stringify(data))
+}
+
+function renderCart(){
+    let cart = JSON.parse(localStorage.getItem("cart"))
+    document.getElementById("productsInCart").innerHTML = ''
+    
+    cart.forEach(element => {
         document.getElementById("productsInCart").innerHTML +=
-        `<tr class="table-light center-table">
-                <td id="trashcan"><button type="button" class="trashcan-center"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+        `<tr id=""class="table-light center-table">
+                <td id="trashcan"><button id="${element.productNummer}" type="button" class="trashcan-center"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
                 <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
               </svg> </td>
                 <td scope="row">${element.title}</td>
@@ -33,7 +43,17 @@ function renderCart(data){
                 <td id="${element.id}">${(element.price * element.quantity).toFixed(2)}</td>
             </tr>`
     });
+
+    $.each($('.trashcan-center'), function(index, e){
+      e.addEventListener('click', e => {
+        console.log(e.target)
+        let removed = cart.filter(product => product.productNummer !== e.target.id)
+        localStorage.setItem("cart", JSON.stringify(removed))
+        renderCart()
+      })
+    })
 }
+
 
 function addProduct(product){
     //uppdatera antalet
@@ -60,7 +80,7 @@ function removeProduct(product) {
   var span = document.getElementsByClassName("close")[0];
   
   btn.onclick = function() {
-    modal.style.display = "block";
+    modal.style.display = "block"
   }
   
   span.onclick = function() {
@@ -74,4 +94,6 @@ function removeProduct(product) {
   }
 
  
-    
+
+ 
+   
