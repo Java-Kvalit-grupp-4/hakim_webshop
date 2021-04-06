e3273c60fbffe437ade96e24453fbfc0edb71711
Christoffer Grännby <cribbster1987@hotmail.com>
2021-04-06 09:35:39 +0200

    small changes

--- pages/checkout/cart-pop-up.js
+++ pages/checkout/cart-pop-up.js
@@ -1,4 +1,7 @@
-let cart = []
+let cart = JSON.parse(localStorage.getItem("cart"));
+if (cart == null) {
+  cart = [];
+}
 
 function load() {
     let cartTestUrl = '../../TestData/test_data_cart.JSON'
@@ -11,10 +14,12 @@ function load() {
 function renderCart(data){
     var getProducts = JSON.parse(localStorage.getItem("cart"))
 
+    // centrera papperskorgen och produktantal i cellerna
+    // ta bort borders på produktantal ??
     data.forEach(element => {
         document.getElementById("productsInCart").innerHTML +=
         `<tr class="table-light center-table">
-                <td><button type="button" class="trashcan-center"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                <td id="trashcan"><button type="button" class="trashcan-center"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
                 <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
               </svg> </td>
                 <td scope="row">${element.title}</td>
@@ -50,26 +55,18 @@ function removeProduct(product) {
       }
     }
 
-      // Get the modal
   var modal = document.getElementById("myModal");
-  
-  // Get the button that opens the modal
   var btn = document.getElementById("myBtn");
-  
-  // Get the <span> element that closes the modal
   var span = document.getElementsByClassName("close")[0];
   
-  // When the user clicks the button, open the modal 
   btn.onclick = function() {
     modal.style.display = "block";
   }
   
-  // When the user clicks on <span> (x), close the modal
   span.onclick = function() {
     modal.style.display = "none";
   }
-  
-  // When the user clicks anywhere outside of the modal, close it
+
   window.onclick = function(event) {
     if (event.target == modal) {
       modal.style.display = "none";
