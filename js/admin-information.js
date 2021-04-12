56f58e6d9666889d2c021c1fe8de201af7e84185
Julia Wigenstedt <juliaemmamatilda@gmail.com>
2021-04-12 18:57:33 +0200

    Add functionality for loading info from test file

--- js/admin-information.js
+++ js/admin-information.js
@@ -1,5 +1,27 @@
-$(function(){
-    console.log("Script is linked");
-    
+$(function () {
+  console.log("Script is linked");
+  addListenerToMenu();
+  function addListenerToMenu() {
+  }
 
-})
+  fetch("../../TestData/test_data_admin_info.json")
+    .then((response) => response.json())
+    .then((response) => renderInformation(response))
+    .catch((error) => console.error(error));
+
+  function renderInformation(response) {
+    console.log(response);
+    Object.entries(response).forEach((element) => console.log(element));
+
+    $("#opening-hours").text(`${response["opening_hours"]}`);
+    $("#deviating-hours").text(`${response["deviating_hours"]}`);
+    $("#info-address:text").val(`${response["street_address"]}`);
+    $("#info-zip:text").val(`${response["zip_code"]}`);
+    $("#info-city:text").val(`${response["city"]}`);
+    $("#info-phone:text").val(`${response["phone_number"]}`);
+    $("#info-email:text").val(`${response["email"]}`);
+    $("#description").text(`${response["about_hakim"]}`);
+
+    $("#about-image").attr("src", `${response["image"]}`);
+  }
+});
