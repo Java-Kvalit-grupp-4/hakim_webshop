af8a8c1afe6fd1118cf504cf8c1009bc5d4016ac
Sara Carlsson <sara.carlsson@yh.nackademin.se>
2021-04-12 17:57:33 +0200

    added function in serachj

--- admin/Customer/admin-customer.js
+++ admin/Customer/admin-customer.js
@@ -1,16 +1,22 @@
 let customers = [];
+let orders = [];
 let choosedCustomer = "";
 let customerFile = "../../TestData/testdata_persons.json";
+let ordersFile = "../../TestData/test_data_orders.json";
 
+$.getJSON(ordersFile, function(response) {
+    console.log(response);
+    orders = response;
+})
 
 $.getJSON(customerFile, function(response) {
     console.log(response);
     customers = response;
-    showCustomers();
+    showCustomers(customers);
 })
 
-function showCustomers(){
-    customers.forEach(e => {
+function showCustomers(customerArr){
+    customerArr.forEach(e => {
         let isVip = "";
         if(e.vip=="true"){
             console.log("ÄR VIP")
@@ -19,15 +25,20 @@ function showCustomers(){
         else{
             isVip = "bi-x"
         }
+        let personalNumber = e.social_security_number;
+        let firstNr = personalNumber.substring(0,6);
+        let lastNr = personalNumber.substring(6);
+
+    
         $("#customerTable").append(`
         <tr>
         <th scope="row"><a href="#nav-contact" class="customer-tab">${e.id}</a></th>
         <td>${e.first_name}</td>
         <td>${e.last_name}</td>
-        <td>${e.social_security_number}</td>
+        <td>${firstNr}-${lastNr}</td>
         <td>${e.email}</td>
-        <td>x</td>
-        <td>x kr</td>
+        <td>${getNumberOfOrders(e.id)}</td>
+        <td>${getTotalPriceOfOrders(e.id).toFixed(2)} kr</td>
         <td><i class="bi ${isVip}"></i></td>
         </tr>
         `
@@ -55,4 +66,52 @@ function saveCustomer(id){
         }
     })
 }
+
+function getNumberOfOrders(id){
+    let sum = 0;
+    orders.forEach(e => {
+        if(e.user.id==id){
+            sum++;
+        }
+    })
+    return sum;
+}
+
+function getTotalPriceOfOrders(id){
+    let sum = 0;
+    orders.forEach(e => {
+        if(e.user.id==id){
+            sum += e.total_cost;
+        }
+    })
+    return sum;
+}
+
+$(document).on('click', '#search-btn', filterSearch);
+
+function filterSearch(){
+    let filter = $("#search-select option:selected").text();
+    let input = $("#input").val();
+    $("#customerTable").empty();
+    switch (filter) {
+        case 'Visa alla':
+            showCustomers(customers);
+            break;
+        case 'Vip-kunder':
+            showCustomers(customers.filter(e => e.vip=="true"));   
+            break;
+        case 'Total ordersumma över:':
+            if(input!=""){
+                showCustomers(customers.filter(e => getTotalPriceOfOrders(e.id)>input));
+            }
+            break;
+        case 'Totalt antal ordrar över:':
+            if(input!=""){
+                showCustomers(customers.filter(e => getNumberOfOrders(e.id)>input));
+            }
+            break;
+        //case 'Födelsedag':
+    }
+};
+
     
