14c0fe49a6e18fd7b7d3cbeca9bd4e05da3d4ae5
Oscar Forss <oscar.forss.1@gmail.com>
2021-04-12 19:36:22 +0200

    render orders to table

--- js/admin-orders.js
+++ js/admin-orders.js
@@ -1,6 +1,6 @@
 let orders = [];
 
-$(function () {
+/* $(function () {
   fetch("../../TestData/test_data_orders.json")
     .then((response) => response.json())
     .then((response) => (orders = response))
@@ -22,4 +22,33 @@ function renderOrders(orders) {
   });
   console.log(output);
   $("#order-output").append(output);
-}
+} */
+
+$(document).ready(() => {
+
+  /**
+   * Cacha variabels
+   */
+  let orderContainer = $('#orders-container')
+
+  let urlToOrders = "../../TestData/test_data_orders.json";
+
+/**
+ * Get All Orders from url
+ */
+$.getJSON(urlToOrders, (response) => {
+  $.each(response, (index, element) => {
+    orderContainer.append(`
+        <tr>
+          <th scope="row" class="ps-md-5"><a href="#">${element.orderId}</a> </th>
+          <th scope="row" class="ps-md-5"><a href="#">${element.user.id}</a> </th>
+          <td>${element.orderTimestamp}</td>
+          <td>${element.totalCost} kr</td>
+          <td>${element.status.type}</td>
+          <td>Mottagen</td>
+        </tr>
+      `
+    )
+  })
+})
+})
