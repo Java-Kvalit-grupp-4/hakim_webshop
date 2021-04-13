6ef3473d5dca39d442aa8bfde5de3d3278c9e7f8
Sara Carlsson <sara.carlsson@yh.nackademin.se>
2021-04-13 16:49:30 +0200

    add function to show/filter date

--- admin/Customer/admin-customer.js
+++ admin/Customer/admin-customer.js
@@ -3,6 +3,8 @@ let orders = [];
 let choosedCustomer = "";
 let customerFile = "../../TestData/testdata_persons.json";
 let ordersFile = "../../TestData/test_data_orders.json";
+let startDate = null;
+let endDate = null;
 
 $.getJSON(ordersFile, function(response) {
     console.log(response);
@@ -15,7 +17,18 @@ $.getJSON(customerFile, function(response) {
     showCustomers(customers);
 })
 
+$(document).ready(function(){
+    $("#reservation").daterangepicker(null, function (start, end, label) {
+        startDate = new Date(start.toISOString())
+        endDate = new Date(end.toISOString())
+    })
+})
+
 function showCustomers(customerArr){
+
+    if(customerArr.length==0){
+        swal("Hittade inga kunder på denna sökning")
+    }
     customerArr.forEach(e => {
         let isVip = "";
         if(e.vip=="true"){
@@ -28,6 +41,7 @@ function showCustomers(customerArr){
         let personalNumber = e.social_security_number;
         let firstNr = personalNumber.substring(0,6);
         let lastNr = personalNumber.substring(6);
+    
         $("#customerTable").append(`
             <tr>
                 <th scope="row"><a href="#" class="customer-tab">${e.id}</a></th>
@@ -35,8 +49,8 @@ function showCustomers(customerArr){
                 <td>${e.last_name}</td>
                 <td>${firstNr}-${lastNr}</td>
                 <td><a href="mailto:${e.email}">${e.email}</a></td>
-                <td>${getNumberOfOrders(e.id)}</td>
-                <td>${getTotalPriceOfOrders(e.id).toFixed(2)} kr</td>
+                <td>${getNumberOfOrders(e.id, orders)}</td>
+                <td>${getTotalPriceOfOrders(e.id, orders).toFixed(2)} kr</td>
                 <td><i class="bi ${isVip}"></i></td>
             </tr>
             `
@@ -46,6 +60,7 @@ function showCustomers(customerArr){
 $(document).on('click', '.customer-tab', openCustomerTab);
 
 function openCustomerTab(){
+    $("#orderTable").empty();
     saveCustomer($(this).text());
     $("#nav-contact-tab").tab('show');
     };
@@ -67,9 +82,9 @@ function saveCustomer(id){
     })
 }
 
-function getNumberOfOrders(id){
+function getNumberOfOrders(id, orderArr){
     let sum = 0;
-    orders.forEach(e => {
+    orderArr.forEach(e => {
         if(e.user.id==id){
             sum++;
         }
@@ -77,9 +92,9 @@ function getNumberOfOrders(id){
     return sum;
 }
 
-function getTotalPriceOfOrders(id){
+function getTotalPriceOfOrders(id, orderArr){
     let sum = 0;
-    orders.forEach(e => {
+    orderArr.forEach(e => {
         if(e.user.id==id){
             sum += e.totalCost;
         }
@@ -90,8 +105,24 @@ function getTotalPriceOfOrders(id){
 $(document).on('click', '#search-btn', filterSearch);
 
 function filterSearch(){
+    let tempOrders = [];
     let filter = $("#search-select option:selected").text();
     let input = $("#input").val();
+  
+    console.log(startDate, endDate);
+
+    if(startDate!=null && endDate!=null){
+        orders.forEach(e => {
+            let date = new Date(e.orderTimestamp);
+            if(date>=startDate && date<=endDate){
+                tempOrders.push(e);
+            }
+        });
+    }
+    else{
+        tempOrders=orders
+    };
+        
     $("#customerTable").empty();
     switch (filter) {
         case 'Visa alla':
@@ -102,21 +133,26 @@ function filterSearch(){
             break;
         case 'Total ordersumma över:':
             if(input!=""){
-                showCustomers(customers.filter(e => getTotalPriceOfOrders(e.id)>input));
+                showCustomers(customers.filter(e => getTotalPriceOfOrders(e.id, tempOrders)>input));
             }
             break;
         case 'Totalt antal ordrar över:':
             if(input!=""){
-                showCustomers(customers.filter(e => getNumberOfOrders(e.id)>input));
+                showCustomers(customers.filter(e => getNumberOfOrders(e.id, tempOrders)>input));
             }
             break;
-        //case 'Födelsedag':
     }
 };
 
 function showOrders(id){
+    let sum = 0;
     orders.forEach(e => {
         if(e.user.id == id){
+            sum += e.totalCost;
+            let dateFromOrder = new Date(e.orderTimestamp);
+            let orderDate = dateFromOrder.toISOString().substring(0,10);
+            console.log(orderDate)
+           
             let isPaid = "Obetalad";
             if(e.isPaid){
                 isPaid ="Betalad"
@@ -125,16 +161,37 @@ function showOrders(id){
                 <tr>
                     <th scope="row" class="col-3">
                     <a href="#">${e.orderId}</a>
-                    </th>
                     <td class="col-2">${e.status.type}</td>
                     <td class="col-2">${isPaid}</td>
-                    <td class="col-3">2021-03-13</td>
-                    <td class="col-2">${e.totalCost}</td>
+                    <td class="col-3">${orderDate}</td>
+                    <td class="col-2">${e.totalCost.toFixed(2)} kr</td>
                 </tr>`
             )
         }
     })
+    $("#totalCost").text(sum.toFixed(2) + " kr");
     
 }
+
+$(document).on('focus', '#search-select', function(){
+    $("#input").val("");
+})
+
+$(document).on('click', '#nav-profile-tab', function(){
+    $("#orderTable").empty();
+    $("#customer-first-name").val("")
+    $("#customer-last-name").val("")
+    $("#customer-email").val("")
+    $("#customer-phone").val("")
+    $("#customer-street").val("")
+    $("#customer-city").val("")
+    $("#customer-zip").val("")
+
+})
+
+
+
+
+        
 
     
