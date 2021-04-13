8a466d001daa7d0e78f213fe00037a5240120c4b
stefan-hennings <stefan.hennings4@gmail.com>
2021-04-13 08:31:21 +0200

    Reverted to old method

--- js/admin-orders.js
+++ js/admin-orders.js
@@ -1,6 +1,6 @@
 let orders = [];
 
- /* $(function () {
+  $(function () {
   fetch("../../TestData/test_data_orders.json")
     .then((response) => response.json())
     .then((response) => (orders = response))
@@ -8,21 +8,18 @@ let orders = [];
 });
 
 function renderOrders(orders) {
-  let output = ""; 
   orders.forEach((element) => {
     const paidString = element.isPaid ? "Mottagen" : "Väntar på betalning";
-    output += `<tr>
+    $("#orders-container").append(`<tr>
           <th scope="row" class="ps-md-5"><a href="#">${element.orderId}</a> </th>
           <th scope="row" class="ps-md-5"><a href="#">${element.user.id}</a> </th>
           <td>${element.orderTimestamp}</td>
           <td>${element.totalCost} kr</td>
           <td>${element.status.type}</td>
           <td>${paidString}</td>
-      </tr>`;
+      </tr>`);
   });
-  console.log(output);
-  $("#order-output").append(output);
-}  */
+}  
 
 /* $(function () {
   fetch("../../TestData/test_data_orders.json")
@@ -49,32 +46,32 @@ function renderOrders(response) {
   $('#orders-container').append(output);
 }  */
 
-$(document).ready(() => {
+// $(document).ready(() => {
 
-  /**
-   * Cacha variabels
-   */
-  let orderContainer = $('#orders-container')
-  let urlToOrders = "../../TestData/test_data_orders.json"
+//   /**
+//    * Cacha variabels
+//    */
+//   let orderContainer = $('#orders-container')
+//   let urlToOrders = "../../TestData/test_data_orders.json"
 
-  /**
-   * Renders response from url
-   */
- $.getJSON(urlToOrders, (response) => {
-  $.each(response, (index, element) => {
-    orderContainer.append(`
-        <tr>
-          <th scope="row" class="ps-md-5"><a href="#">${element.orderId}</a> </th>
-          <th scope="row" class="ps-md-5"><a href="#">${element.user.id}</a> </th>
-          <td>${element.orderTimestamp}</td>
-          <td>${element.totalCost} kr</td>
-          <td>${element.status.type}</td>
-          <td>Mottagen</td>
-        </tr>
-      `
-    )
-  })
-})
-})  
+//   /**
+//    * Renders response from url
+//    */
+//  $.getJSON(urlToOrders, (response) => {
+//   $.each(response, (index, element) => {
+//     orderContainer.append(`
+//         <tr>
+//           <th scope="row" class="ps-md-5"><a href="#">${element.orderId}</a> </th>
+//           <th scope="row" class="ps-md-5"><a href="#">${element.user.id}</a> </th>
+//           <td>${element.orderTimestamp}</td>
+//           <td>${element.totalCost} kr</td>
+//           <td>${element.status.type}</td>
+//           <td>Mottagen</td>
+//         </tr>
+//       `
+//     )
+//   })
+// })
+// })  
 
 
