81f3afa5c829cbe04ca9a4e09c887e5940451df6
Oscar Forss <oscar.forss.1@gmail.com>
2021-04-17 22:12:58 +0200

    Clean up from requested change on pull request

--- js/searchBar.js
+++ js/searchBar.js
@@ -1,98 +1,124 @@
-// getting all required elements
-const searchWrapper = document.querySelector(".search-input");
-const inputBox = searchWrapper.querySelector("input");
-const suggBox = searchWrapper.querySelector(".autocom-box");
-const icon = searchWrapper.querySelector(".icon");
-let linkTag = searchWrapper.querySelector("a");
-let webLink;
 
-let allProductsurl = `http://localhost:8080/products`
-allProductsurl = './TestData/test_data_products_v1.2.JSON'
+const searchWrapper = $('.search-input') 
+const searchField =  $('#search-field') 
+const suggBox = $('.autocom-box')
+const searchIcon = $('.icon')  
+
+let allProductsUrl = `http://localhost:8080/products`
 let searchWords = []
 
+/**
+ * Eventlisteners
+ */
 
-function getProductsFromDataBase() {
-    axios.get(allProductsurl)
-    .then(response => {
-        getSearchWords(response.data)
-    })
-    .catch(err => alert(err))
-
-}
-
-function getSearchWords(data) {
-    data.forEach(element => {
-        console.log(element)
-        let searchStringToSplit = `${element.title} ${element.description}`
-        element.categories.forEach(e => {
-            searchStringToSplit += ` ${e.category}`
-        })
-        element.tags.forEach(e => {
-            searchStringToSplit += ` ${e.tagName}`
-        })
-        searchWords = searchStringToSplit.split(' ') 
-    });
-}
-
-getProductsFromDataBase()
-
-let index;
-
-// if user press any key and release
-inputBox.onkeyup = (e)=>{
-    
-    let userData = e.target.value; //user enetered data   
-    let emptyArray = [];
-    let allList = [];
-    if(userData){
+/**
+ * Checks for keyPresses in the searchField
+ */
+searchField.keyup((e)=>{
+    let userInput = e.target.value //user enetered data   
+    let emptyArray = []
+    let allList = []
+    if(userInput){
         emptyArray = searchWords.filter((data)=>{
             //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
-            return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase()); 
+            return data.toLocaleLowerCase().startsWith(userInput.toLocaleLowerCase()); 
         });
         emptyArray = emptyArray.map((data)=>{
             // passing return data inside li tag
-            return data = '<li>'+ data +'</li>';
+            return data = '<li>'+ data +'</li>'
         });
-        searchWrapper.classList.add("active"); //show autocomplete box
-        showSuggestions(emptyArray);
-        allList = suggBox.querySelectorAll("li");
+        searchWrapper.addClass("active") //show autocomplete box
+        showSuggestions(emptyArray)
+        allList = suggBox.children("li")
         for (let i = 0; i < allList.length; i++) {
             //adding onclick attribute in all li tag
             allList[i].setAttribute("onclick", "select(this)")
         }
     }else{
-        searchWrapper.classList.remove("active"); //hide autocomplete box
+        searchWrapper.removeClass("active"); //hide autocomplete box
     }
 
+})
+
+/**
+ * Gets the products from the database
+ */
+function getProductsFromDataBase() {
+    axios.get(allProductsUrl)
+    .then(response => {
+        createSearchWords(response.data) // use this if you fetching from database -> response.data
+    })
+    .catch(err => alert(err))
 }
 
+/**
+ * Take an array of products, gets the product title,
+ * product categories, product tags, and the product 
+ * discription and puts in an array
+ * @param {array} products 
+ */
+function createSearchWords(products) {
+    let searchStringToSplit
+
+    products.forEach(product => {
+        searchStringToSplit = `${product.title} ${product.description}`
+        product.categories.forEach(category => {
+            searchStringToSplit += ` ${category.category}`
+        })
+        product.tags.forEach(tag => {
+            searchStringToSplit += ` ${tag.tagName}`
+        }) 
+    })
+    
+    searchWords = searchStringToSplit.split(' ')
+}
+
+/**
+ * Gets the text from the selected element
+ * and puts it in the searchfield 
+ * Send request for server if icon is pressed
+ * 
+ * @param {HTML element} element 
+ */
 function select(element){
-    let selectData = element.textContent;
-    inputBox.value = selectData;
-    icon.onclick = ()=>{
-        console.log(selectData);
+    let selectData = element.innerText
+    searchField.val(selectData)
+    searchIcon.click(() =>{
         sendDataToServer(selectData)
-    } 
-    searchWrapper.classList.remove("active");
+    })
+    searchWrapper.removeClass("active")
 }
 
+/**
+ * Takes an array of data and show 
+ * puts it in the suggestion box underthe 
+ * searchfield
+ * @param {String[]} list 
+ */
 function showSuggestions(list){
-    index = list.length - list.length -1
-    let listData;
+    index = -1
+    let listData
     if(!list.length){
-        userValue = inputBox.value;
-        listData = '<li>'+ userValue +'</li>';
+        userValue = searchField.val()
+        listData = '<li>'+ userValue +'</li>'
     }else{
-        listData = list.join('');
+        listData = list.join('')
     }
-    suggBox.innerHTML = listData;
+    suggBox.html(listData)
 }
+
+/**
+ * Take the users searchword and send it to 
+ * the database for a response. Gets an array
+ * of product that matches the word and send it
+ * to getProducts() for render on the screen
+ * @param {String} searchWord 
+ */
 
 function sendDataToServer(searchWord) {
     let productMatchWordUrl = `http://localhost:8080/products/searchProducts?searchWord=${searchWord}`
     axios.get(productMatchWordUrl)
     .then(response => {
-        console.log(response)
         getProducts(response.data)
         // rendera producterna du sökt efter med response.data
     })
@@ -100,31 +126,4 @@ function sendDataToServer(searchWord) {
 }
 
 
-axios.get('./TestData/test_data_orders.json')
-.then(response =>  getOrderStatus(response.data))
-
-function getOrderStatus(data) {
-    let status1 = 0,
-    status2 = 0,
-    status3 = 0,
-    status4 = 0,
-    status5 = 0
-
-    data.forEach(e => {
-        console.log(e);
-        switch(e.status.type.toLowerCase()) {
-            case 'väntar på behandling': status1 += 1; break;
-            case 'plockning pågår': status2 += 1; break;
-            case 'redo att skickas': status3 += 1 ; break;
-            case 'skickad': status4 += 1; break;
-            case 'avbeställd': status5 += 1; break;
-        }
-    })
-
-    $('#status1').text(status1)
-    $('#status2').text(status2)
-    $('#status3').text(status3)
-    $('#status4').text(status4)
-    $('#status5').text(status5)
-
-}
+getProductsFromDataBase()
