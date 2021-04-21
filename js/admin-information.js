f21641ffa12ca2228c7b639ed1c77ad584b446ae
Julia Wigenstedt <juliaemmamatilda@gmail.com>
2021-04-21 10:35:18 +0200

    Add first attempt to post information to server through axios

--- js/admin-information.js
+++ js/admin-information.js
@@ -1,28 +1,36 @@
 const testData = "../../TestData/test_data_admin_info.json";
 const localHost = "http://localhost:8080/information";
+const heroku = "https://hakimlivs.herokuapp.com/information";
+const imageSource = "https://hakimlivs.herokuapp.com/api/v1/images/";
+let informationObject = new Object();
+const chosenImageName = "Hakim.jpg";
 
 $(function () {
-  fetch(localHost)
+  fetch(heroku)
     .then((response) => response.json())
     .then((response) => renderInformation(response))
     .catch((error) => console.error(error));
 
-  // var outside;
-
-  // fetch("http://localhost:8080/api/v1/image?imageUrl=uploaded_Hakim.jpg")
-  //   .then((response) => response.blob())
-  //   .then((images) => {
-  //     // Then create a local URL for that image and print it
-  //     outside = URL.createObjectURL(images);
-  //     console.log(outside);
-  //   });
-
   function renderInformation(response) {
-    console.log(response);
-    let imageSource = "http://localhost:8080/api/v1/image?imageUrl=";
+    informationObject = {
+      openingHours: response[0].openingHours,
+      deviatingHours: response[0].deviatingHours,
+      streetAddress: response[0].streetAddress,
+      zipCode: response[0].zipCode,
+      phoneNumber: response[0].phoneNumber,
+      email: response[0].email,
+      aboutInfo: response[0].aboutInfo,
+      imageUrl: response[0].imageUrl,
+    };
+    console.log(informationObject);
 
+    //This fetches images from local directory. Will not work on Heroku
+    // let imageSource = "http://localhost:8080/api/v1/image?imageUrl=";
+
+    //This mapping gets files from db by file name.
+    let imageLink = imageSource;
     response.forEach((element) => {
-      imageSource += element.imageUrl;
+      imageLink += element.imageUrl;
       console.log(imageSource);
       console.table(element);
       console.log(element.openingHours);
@@ -38,51 +46,140 @@ $(function () {
       $("#info-phone:text").val(`${element.phoneNumber}`);
       $("#info-email:text").val(`${element.email}`);
       $("#description").text(`${aboutInfo}`);
-
-      // $("#about-image").attr("src", `${imageSource}`);
-
-      console.log($("#opening-hours").val());
     });
 
-    loadImage(imageSource);
-
-    // $("#deviating-hours").text(`${response["deviating_hours"]}`);
-    // $("#info-address:text").val(`${response["street_address"]}`);
-    // $("#info-zip:text").val(`${response["zip_code"]}`);
-    // $("#info-city:text").val(`${response["city"]}`);
-    // $("#info-phone:text").val(`${response["phone_number"]}`);
-    // $("#info-email:text").val(`${response["email"]}`);
-    // $("#description").text(`${response["about_hakim"]}`);
-
-    // $("#about-image").attr("src", `${response["image"]}`);
+    localStorage.setItem("imageUrl", response[0].imageUrl);
+    loadImage(imageLink);
   }
   $('input[type="file"]').change(function (e) {
-    var fileName = e.target.files[0].name;
+    let fileName = e.target.files[0].name;
     let fileData = e.target.files[0];
     console.log(fileData);
     console.log('The file "' + fileName + '" has been selected.');
-    
   });
-$("#uploadButton").on("click", function() {
-  uploadImage();
-})
 
+  $("#uploadButton").on("click", function () {
+    uploadImage();
+  });
+
+  $("#btnOpeningHours").on("click", function () {
+    
+    let data = getInformationFromFields();
+    console.log(data);
+    
+    axios
+      .post("https://hakimlivs.herokuapp.com/information/update", data, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then(() => {
+        swal(
+          "Informationen har uppdaterats",
+          "Uppladdningen lyckades",
+          "success"
+        );
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+        swal("Något fick fel!", "Vänligen försök igen", "warning");
+      });
+  });
 
   function uploadImage() {
-  var formData = new FormData();
-  var imagefile = document.querySelector("#fileUpload");
-  formData.append("image", imagefile.files[0]);
-  // axios.post("upload_file", formData, {
-  //   headers: {
-  //     "Content-Type": "multipart/form-data",
-  //   },
-  // }); 
-  console.log(formData);
+    const newFileName = "Hakim.jpg";
+    let formData = new FormData();
+    let imagefile = document.querySelector("#fileUpload");
+    if (imagefile.files[0] != undefined) {
+      let selectedImageFile = imagefile.files[0];
+      formData.append("file", selectedImageFile, newFileName);
+      axios
+        .post("https://hakimlivs.herokuapp.com/api/v1/upload/db", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        })
+        .then(() => {
+          swal("Ny bild uppladdad", "Uppladdningen lyckades", "success")
+            .then(() => localStorage.setItem("imageUrl", newFileName))
+            .then(() => loadImage(imageSource + newFileName));
+        })
+        .catch(() => {
+          swal("Något fick fel!", "Vänligen försök igen", "warning");
+        });
+    }
   }
-
-  
 
   function loadImage(imageUrl) {
     $("#about-image").attr("src", `${imageUrl}`);
+  }
+
+  function removeImage(imageUrl) {
+    axios.get("https://hakimlivs.herokuapp.com/api/v1/delete/" + imageUrl);
+    // .then(() => {
+    //   swal("Bilden är borttagen");
+    // })
+    // .then(() => {
+    //   resetImage();
+    // })
+    // .catch(() => {
+    //   swal("Något gick fel!", "Vänligen försök igen", "warning");
+    // });
+  }
+
+  $("#removeImageButton").on("click", function () {
+    const imageUrl = localStorage.getItem("imageUrl");
+
+    swal({
+      title: "Är du säker?",
+      text: "Det går inte att ångra detta val!",
+      icon: "warning",
+      buttons: ["Avbryt", "Bekräfta"],
+      dangerMode: true,
+    }).then((willDelete) => {
+      if (willDelete) {
+        swal({
+          title: "Bilden är borttagen",
+          icon: "success",
+        })
+          .then(() => {
+            return removeImage(imageUrl);
+          })
+          .then(() => {
+            resetImage();
+          });
+      } else {
+        swal("Radering avbruten!");
+      }
+    });
+    removeImage(imageUrl);
+  });
+
+  function resetImage() {
+    $("#about-image").attr("src", "");
+    localStorage.removeItem("imageUrl");
+  }
+
+  function getInformationFromFields() {
+    const openingHours = $("#opening-hours").val().replaceAll("\n", "/").trim();
+    const deviatingHours = $("#deviating-hours").val().replaceAll("\n", "/");
+    const streetAddress = $("#info-address:text").val();
+    const zipCode = $("#info-zip:text").val();
+    const city = $("#info-city:text").val();
+    const phoneNumber = $("#info-phone:text").val();
+    const email = $("#info-email:text").val();
+    const aboutInfo = $("#description").val().replaceAll("\n", "/");
+
+    informationObject = {
+      openingHours: openingHours,
+      deviatingHours: deviatingHours,
+      streetAddress: streetAddress,
+      zipCode: zipCode,
+      phoneNumber: phoneNumber,
+      email: email,
+      aboutInfo: aboutInfo,
+      imageUrl: chosenImageName,
+    };
+    return informationObject;
   }
 });
