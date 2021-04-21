a5772c6764f4502601c44d8d847919fccdd1b98c
I-Zor <ivona.zoricic@gmail.com>
2021-04-21 13:29:19 +0200

    Not finished

--- js/admin-products.js
+++ js/admin-products.js
@@ -1,13 +1,10 @@
 $(document).ready(loadProducts);
 
-let products = [];
-let categories = [];
+let products = []
+let categories = []
+let uniqueCategories = []
 
 function loadProducts() {
-  /* fetch("../../TestData/test_data_products_v1.2.JSON")
-    .then((response) => response.json())
-    .then((data) => render(data))
-    .catch((error) => console.error(error)); */
   axios.get("http://localhost:8080/products")
     .then((response) => {
       console.log(response.data)
@@ -25,12 +22,10 @@ function loadProducts() {
 
   function render(products) {
 
-    let uniqueCategories = []
+    
     products.forEach(element => {
-      //  console.log(element.categories.find(o => o.category).category)
       for (let i = 0; i < element.categories.length; i++) {
         let obj = element.categories[i]
-      //  console.log(obj.category)
         categories.push(obj.name)
       }
     });
@@ -49,12 +44,13 @@ function loadProducts() {
       );
     });
 
+    showCategories()
+
     /**
      * Add products that belong to the celected category
      */
     $("#select option").on("click", function () {
       let optionId = $(this).attr("id");
-      console.log(optionId)
       let list = [];
       products.forEach(element => {
         for (let i = 0; i < element.categories.length; i++) {
@@ -95,7 +91,7 @@ function loadProducts() {
     let productId = "";
     $("#products").on("click", "tr", function () {
       $(this).addClass("highlight").siblings().removeClass("highlight");
-      console.log(productId = $(this).attr("id"));
+      productId = $(this).attr("id");
     });
 
     /**
@@ -107,7 +103,7 @@ function loadProducts() {
       $("#column").empty();
 
       products.forEach(element => {
-        if (element.id == productId) {
+        if (element.sku == productId) {
           $("#title").val(element.title);
           $("#brand").val(element.brand.name);
           $("#description").val(element.description);
@@ -115,21 +111,14 @@ function loadProducts() {
           $("#sku").val(element.sku);
           $("#price").val(element.price);
           $("#lager").val(element.quantity);
-          uniqueCategories.forEach(element => {
-            $("#column").append(`
-                <div id="${element}" class="form-check">
-                            <input class="form-check-input me-3" type="checkbox" value="" id="${element}">
-                            <label class="form-check-label" for="cat1">${element}</label>
-                        </div>
-                `)
-          });
+          showCategories()
         }
 
         $("#column div").filter(function () {
           for (let i = 0; i < element.categories.length; i++) {
             let obj = element.categories[i]
           
-          if (element.id == productId && obj.name == $(this).attr("id")) {
+          if (element.sku == productId && obj.name == $(this).attr("id")) {
             $(this).replaceWith(function () {
               return `<div class="form-check">
                <input class="form-check-input me-3" type="checkbox" value="" id="${obj.name}" checked>
@@ -158,6 +147,7 @@ function loadProducts() {
       $("#price").val("");
       $("#lager").val("");
       $("input").prop("checked", false);
+      
       $("#tab-product-site").tab("show");
     });
 
@@ -175,33 +165,34 @@ function loadProducts() {
       })
 
           let productObject = {
-            sku: $("#sku").val(),
-            description: $("#description").val(),
-            image: $("#img").val(),
-            is_available: true,
-            price: $("#price").val(),
-            quantity: $("#lager").val(),
-            title: $("#title").val(),
-            brand: $("#brand").val(),
-            tags: $("#tags").val(),
-            categories: cat
+            "sku": $("#sku").val(),
+            "description": $("#description").val(),
+            "image": $("#img").val(),
+            "is_available": true,
+            "price": $("#price").val(),
+            "quantity": $("#lager").val(),
+            "title": $("#title").val(),
+            "brand": {
+              "name": $("#brand").val()
+            },
+            "tags": $("#tags").val(),
+            
+            "categories": [{
+              "name" : cat[0]
+              }]
           };
 
         console.log(productObject)
       alert("Produkten har sparats")
         
-  
-     
-
-      
-       axios.post("http://localhost:8080/products/add", productObject)
+      axios.post("http://localhost:8080/products/add",  productObject  )
         .then(() => {
           console.log("Done!")
         })
         .catch(() => {
           alert('Något fick fel!','Vänligen försök igen', 'warning')
         })
-     });
+      });
 
   }
 }
@@ -213,7 +204,7 @@ function loadProducts() {
 function showProducts(l) {
   l.forEach(element => {
     $("#products").append(
-      `<tr id="${element.id}">
+      `<tr id="${element.sku}">
             <td>${element.sku}</td>
             <td ><h5 >${element.title}</h5></td>
             <td ><h5 >${element.brand.name}</h5></td>
@@ -222,6 +213,18 @@ function showProducts(l) {
             </tr>`
     )
   });
+}
+
+function showCategories() {
+  uniqueCategories.forEach(element => {
+    $("#column").append(`
+        <div id="${element}" class="form-check">
+                    <input class="form-check-input me-3" type="checkbox" value="" id="${element}">
+                    <label class="form-check-label" for="cat1">${element}</label>
+                </div>
+        `)
+  });
+
 }
 
 
