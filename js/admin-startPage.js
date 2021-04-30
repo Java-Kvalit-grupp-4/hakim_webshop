e66d5f93f29a56d5fbe648d9dec78374c25fb80d
Sara Carlsson <sara.carlsson@yh.nackademin.se>
2021-04-30 14:49:26 +0200

    update to get info from database on admin-homepage

---
+++ js/admin-startPage.js
@@ -0,0 +1,54 @@
+$(function(){
+
+    axios.get("https://hakimlivs.herokuapp.com/customerOrder/orders")
+    .then(response => {
+        getOrderStatus(response.data)
+        //console.log(response.data)
+    })
+
+    function getOrderStatus (data) {
+        
+        let waiting = 0;
+        let picking = 0;
+        let ready = 0;
+        let sent = 0;
+        let canceled = 0;
+            data.forEach(element => {
+                console.log(element.orderStatus.type)
+                switch(element.orderStatus.type){
+                    case "Ohanterad": waiting += 1;  break;
+                    case "Plockning pågår": picking += 1;       break; 
+                    case "Skickad": sent += 1;                  break;              
+                    case "Avbeställd": canceled += 1;           break;  
+                }
+            })
+            $('#waitingForHandling').text(waiting)
+            $('#pickingInProgress').text(picking)
+            $('#readyForSending').text(ready)
+            $('#sent').text(sent)
+            $('#canceled').text(canceled)
+        }
+
+    axios.get("https://hakimlivs.herokuapp.com/products")
+    .then(response => {
+        getOutOfStockProducts(response.data)
+        
+    })
+
+    function getOutOfStockProducts(data){
+        console.log(data)
+
+        data.forEach(product =>{
+            if(product.quantity<1){
+                $("#products").append(`
+                <tr>
+                <th><a href="#">${product.sku}</a></th>
+                <td>${product.title}</td>
+                <td>${product.brand.name}</td>
+                <td>${product.price}</td>
+              </tr>`)
+            }
+        })
+    }
+
+})
