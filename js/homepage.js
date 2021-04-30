ff1eba896ed78e6d9ddf59468e1672f4094366ae
PatrikJMelander <patrikjmelander@gmail.com>
2021-04-30 13:28:42 +0200

    change variable in render products

--- js/homepage.js
+++ js/homepage.js
@@ -163,25 +163,17 @@ function renderProducts(list) {
 
   // add product to website  
   list.forEach((element) => {
-      let unit = 'hg'
       let unitCheck = ''
-      let volym = 33
-      let comparePrice = '35,00'
+
 
       // lägg till element framför för att jämföra med databasen
-      if(unit == 'ml' || unit == 'cl' || unit == 'l'){
-          unitCheck = 'l'
-      }
-      if(unit == 'gr' || unit == 'hg' || unit == 'kg'){
-        unitCheck = 'kg'
-      }
 
-        /* if(element.unit == 'ml' || element.unit == 'cl' || element.unit == 'l'){
+        if(element.unit == 'ml' || element.unit == 'cl' || element.unit == 'l'){
             unitCheck = 'l'
         }
         if(element.unit == 'gr' || element.unit == 'hg' || element.unit == 'kg'){
           unitCheck = 'kg'
-        } */
+        } 
 
     $("#products").append(`
         <div class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3">
@@ -190,7 +182,7 @@ function renderProducts(list) {
                     <div class="img-container">
                     <img src="${element.image}" alt="img" class="product-card-img">
                     </div>
-                    <div id="${unit}" class="product-card-text">
+                    <div id="${element.unit}" class="product-card-text">
                     <h3 class="card-title">${element.title}</h3>
                     <h5 class="card-price">${element.price.toLocaleString(
                         "sv-SE",
@@ -199,7 +191,7 @@ function renderProducts(list) {
                         maximumFractionDigits: 2,
                         }
                     )} kr</h5>
-                    <p id="${volym}" class="card-comp-price">jfr-pris ${comparePrice} kr/${unitCheck}</p>
+                    <p id="${element.volume}" class="card-comp-price">jfr-pris ${element.comparablePrice} kr/${unitCheck}</p>
                     <p id="${element.description}"class="card-text">Mer info om produkten</p>
                     <div class="add-subtract-container">
                         <div class="subtract-btn">
