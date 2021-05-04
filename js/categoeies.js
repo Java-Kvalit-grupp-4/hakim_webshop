b76d7e062496a8effb6843fb04d7f0e235a79873
Julia Wigenstedt <juliaemmamatilda@gmail.com>
2021-05-04 23:26:24 +0200

    Add more styling to home page

--- js/categoeies.js
+++ js/categoeies.js
@@ -2,9 +2,10 @@
 $('.all-categories').click(function(){
 
     $('.categories-dropdown-list').toggleClass('open')
-
+$("#sidomeny").css("overflow: auto");
     $('.categories-dropdown-list.open').click(() => {
         $('.categories-dropdown-list').removeClass('open')
+        $("#sidomeny").css("overflow: hidden");
         
     })
 });
